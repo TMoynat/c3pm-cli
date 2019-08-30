3c761c509621f00f7ab83dab1b9c6a5e88a9c3bd
tmoynat <thibault.moynat@epitech.eu>
2019-08-31 00:17:10 +0200

    [Add] implem prompt init

--- cmd/init.go
+++ cmd/init.go
@@ -2,9 +2,11 @@ package cmd
 
 import (
 	"fmt"
+	"errors"
+	"os"
 
 	"github.com/spf13/viper"
-
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 
 	"ctpm/constants"
@@ -20,8 +22,58 @@ var initCmd = &cobra.Command{
 		viper.Set("name", "this_is_a_test")
 		_ = viper.WriteConfigAs(constants.ConfigurationFileName)
 
+		initProject()
 		return nil
 	},
+}
+
+func initProject() {
+	validateN := func(input string) error {
+		if len(input) < 1 {
+			return errors.New("Invalid name")
+		}
+		return nil
+	}
+
+	validateP := func(input string) error {
+		err := os.Mkdir(input, 0755)
+		if err != nil {
+			return errors.New("Invalid path")
+		}
+		return nil
+	}
+
+	name := promptui.Prompt{
+		Label:    "Project name ",
+		Validate: validateN,
+	}
+
+	pName, err := name.Run()
+	checkError(err)
+
+	author := promptui.Prompt{
+		Label:    "Author ",
+	}
+
+	pAuth, err := author.Run()
+	checkError(err)
+
+	path := promptui.Prompt{
+		Label:    "Project location ",
+		Validate: validateP,
+	}
+
+	pPath, err := path.Run()
+	checkError(err)
+
+	fmt.Println("You choose : ", pName, pAuth, pPath)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(constants.CommandExitStatus)
+	}
 }
 
 func init() {
